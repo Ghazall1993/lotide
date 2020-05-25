45e6729c10b28e7b5f79107a5435a7d584837a21
Ghazal <ghazal.fouladfard@gmail.com>
2020-05-25 20:58:22 +0000

    update code using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const assertEqual = function(actual, expected) {
   const PS1 = "✅ ";
   const PS2 = "🛑 ";
   if (actual === expected)
-    console.log(PS1 + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${PS1} Assertion Passed: ${actual} === ${expected}`);
   else
-    console.log(PS2 + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${PS2} Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
