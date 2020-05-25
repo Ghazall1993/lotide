6c304daca453611bb852e93214101e78ed98658e
Ghazal <ghazal.fouladfard@gmail.com>
2020-05-25 20:35:23 +0000

    add assertion equal challenge

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const PS1 = "✅ ";
+  const PS2 = "🛑 ";
+  if (actual === expected)
+    console.log(PS1 + "Assertion Passed: " + actual + " === " + expected);
+  else
+    console.log(PS2 + "Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("lighthouse", "lighthouse");
+assertEqual(10, 1);
