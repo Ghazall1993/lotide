446b4b39bfb3ae49cd14799714e2619bd76e2f4f
Ghazal <ghazal.fouladfard@gmail.com>
2020-05-26 18:32:52 +0000

    implement challenge assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const eqArrays = function(a , b) {
+  return (JSON.stringify(a) === JSON.stringify(b));
+};
+
+const assertArraysEqual = function (a , b) {
+  const PS1 = "✅ ";
+  const PS2 = "🛑 ";
+  let res = "";
+  const equal = eqArrays(a,b);
+  return (equal ? console.log(`${PS1} Assertion Passed!`) : console.log(`${PS2} Assertion Failed!`));
+};
+
+assertArraysEqual ([2, 1, 3], [1, 2, 3]);
