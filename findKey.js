f7d946ea05b5bdd1f03c1b74e5cf24ab050ef749
vagrant <ghazal.fouladfard@gmail.com>
2020-05-28 23:11:58 +0000

    implement findKey function

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  const PS1 = "✅ ";
+  const PS2 = "🛑 ";
+  if (actual === expected)
+    console.log(`${PS1} Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`${PS2} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(object,callback) {
+  for (let property in object) {
+    if (callback(object[property]))
+      return property;
+  }
+};
+
+const a = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+//test
+assertEqual(a , "noma");
+
