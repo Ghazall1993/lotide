976cfbd9f68501ab6883b40d150f3d127b0f51aa
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-01 04:34:11 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const map   = require('./map');
+const middle   = require('./middle');
+const letterPositions   = require('./letterPositions');
+const countLetters   = require('./countLetters');
+
+
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  map: map,
+  middle: middle,
+  letterPositions:letterPositions,
+  countLetters: countLetters
+};
