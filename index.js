44b223eacc84923e0c15cd434aadc4e1615774ad
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-01 15:08:51 +0000

    add new modules

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const middle = require('./middle');
 const map   = require('./map');
 const without   = require('./without');
 const letterPositions   = require('./letterPositions');
+const countOnly   = require('./countOnly');
 const countLetters   = require('./countLetters');
 const assertArraysEqual = require('./assertArraysEqual')
 const assertEqual = require('./assertEqual')
 const assertObjectsEqual = require('./assertObjectsEqual')
+const findKeyByValue = require('./findKeyByValue')
+const eqObjects = require('./eqObjects')
+const takeUntil = require('./takeUntil')
+const findKey = require('./findKey')
 
 
 module.exports = {
@@ -16,9 +21,14 @@ module.exports = {
   middle,
   map,
   letterPositions,
+  countOnly,
   countLetters,
+  eqObjects,
   assertArraysEqual,
   assertEqual,
   assertObjectsEqual,
-  without
+  without,
+  findKeyByValue,
+  findKey,
+  takeUntil
 };
