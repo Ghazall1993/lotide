5212e4b6d2eed845b852fd58edb4f5acbb09a424
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-01 03:12:24 +0000

    require assertEqual.js

--- tail.js
+++ tail.js
@@ -1,12 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual,expected) {
-  const PS1 = "✅ ";
-  const PS2 = "🛑 ";
-  if (actual === expected)
-    console.log(`${PS1} Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`${PS2} Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   return arr.slice(1,arr.length);
