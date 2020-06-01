bcafe0e314799d7bc119e83f20e9f2c75086ee14
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-01 14:47:14 +0000

    add modules to index.js and update info in package.json

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const map   = require('./map');
+const without   = require('./without');
 const letterPositions   = require('./letterPositions');
 const countLetters   = require('./countLetters');
-
-
+const assertArraysEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const assertObjectsEqual = require('./assertObjectsEqual')
 
 
 module.exports = {
@@ -14,5 +16,9 @@ module.exports = {
   middle,
   map,
   letterPositions,
-  countLetters
+  countLetters,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  without
 };
